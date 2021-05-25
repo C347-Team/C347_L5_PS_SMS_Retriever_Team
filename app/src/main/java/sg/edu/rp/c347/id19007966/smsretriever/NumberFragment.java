8ce08a412dafb1b20ab5005e244d81a6ca7973cc
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-05-25 12:22:31 +0800

    base setup

---
+++ app/src/main/java/sg/edu/rp/c347/id19007966/smsretriever/NumberFragment.java
@@ -0,0 +1,19 @@
+package sg.edu.rp.c347.id19007966.smsretriever;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NumberFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_number, container, false);
+    }
+}
